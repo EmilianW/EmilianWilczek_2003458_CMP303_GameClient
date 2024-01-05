345b14a534fc32d2b0a1fba6c83536b31fd966f4
EmilianW <40626246+EmilianW@users.noreply.github.com>
2024-01-05 11:07:36 +0100

    Interpolation added to PlayerManager.cs

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,6 +1,9 @@
 // Emilian Wilczek 2003458
 // Written following a Unity C# Networking tutorial by Tom Weiland
 
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
@@ -10,12 +13,26 @@ public class PlayerManager : MonoBehaviour
     public float health;
     public float maxHealth = 100f;
     public MeshRenderer model;
+    private Vector3 fromPos = Vector3.zero;
+    private Vector3 toPos = Vector3.zero;
+    private float lastTime;
 
     public void Initialize(int _id, string _username)
     {
         id = _id;
         username = _username;
         health = maxHealth;
+    }
+
+    public void SetPosition(Vector3 pos)
+    {
+        fromPos = toPos;
+        toPos = pos;
+        lastTime = Time.time;
+    }
+    private void Update()
+    {
+        this.transform.position = Vector3.Lerp(fromPos, toPos, (Time.time - lastTime) / (1.0f / 30));
     }
 
     public void SetHealth(float _health)
